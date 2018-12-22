eb9e508415efd6327726786ea10f4a264cbb808c
tuan <canhazn@gmail.com>
2018-12-22 14:33:09 +0700

    back up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,38 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, group, query, style, animate, state } from '@angular/animations';
+import { stagger, trigger, transition, group, query, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-    trigger('openClose', [
-      // ...
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.5,
-        backgroundColor: 'green'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-    ]),
-  ],
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0, transform: 'translateY(40px)'})),
+        query(':enter', [
+          stagger(100, [
+            animate(300, style({opacity: 1, transform: 'translateY(0)'}))
+          ])
+        ]),
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
-  isOpen = true;
- 
-  toggle() {
-    this.isOpen = !this.isOpen;
-  }
+
+  array = ["nguyen", "tuan", "anh", "ok", "anh", "day"];
+
   constructor() { }
 
   ngOnInit() {
